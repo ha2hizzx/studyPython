ce5d683a1039ac56f72dc0ffc032291c3b27aae9
skwenr <doddessto@gmail.com>
2023-05-11 23:56:16 +0900

    datatype_int_float_str

--- datatype.py
+++ datatype.py
@@ -23,3 +23,21 @@ print(a / b) #나누기 3 / 4 = 0.75
 print(a // b) #나누기(몫) 3 // 4 = 0
 print(a ** b) #제곱 3 ** 4 = 81
 print(a % b) #나누기(나머지) 3 % 4 = 3
+
+#문자열 자료형
+a = "Hello world"
+print(a)
+print(type(a)) #str은 물자열 자료형이라는 뜻이다
+a = "'python' is best" # "" 안에 '' 을 넣을수있다
+print(a)
+#탈출문자
+a = "Hello \"python\"" # \ 을 사용하여 문자열 "" 와 string의 시작과 끝을 구분지을수 있다.
+print(a) 
+a = """ Hi  
+   you can not be side
+             I love python""" # """ """ 안에 있는 텝과 간격등을 모두 인식한다. 탈출문자를 쓰지않아도 된다.
+print(a)
+a = "hello "
+b = "python"
+print(a * 5) # a를 5번 출력한다.
+print(a + b) # a와 b를 합쳐서 출력한다.
