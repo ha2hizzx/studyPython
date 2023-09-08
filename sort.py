ac991234e868c147f061bde72c67d24e96dabb95
skwenr <doddessto@gmail.com>
2023-09-08 11:15:14 +0900

    sort

--- sort.py
+++ sort.py
@@ -40,3 +40,10 @@ student_tuples = [
     ('Dave', 'B', 10),
 ]
 sorted(student_tuples, key=lambda student: student[2])
+class Student:
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+    def __repr__(self):
+        return repr((self.name, self. grade, self.age))
